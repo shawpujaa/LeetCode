b2ac3a0d0c4be9cdccf6a73c65dfc2278abc3e5f
PUJA SHAW <95918174+shawpujaa@users.noreply.github.com>
2022-03-27 20:36:50 +0530

    Time: 8 ms (55.76%), Space: 9 MB (27.02%) - LeetHub

---
+++ 942-di-string-match/942-di-string-match.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int low=0, high=s.size(), n=s.size();
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='I')
+            {
+                ans.push_back(low);
+                low++;
+            }
+            else
+            {
+                ans.push_back(high);
+                high--;
+            }
+        }
+        
+        ans.push_back(low);
+        
+        return ans;
+    }
+};
