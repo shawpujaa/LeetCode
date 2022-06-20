75988363be0039a985c1bd8ff44ecf985b6fd98f
PUJA SHAW <95918174+shawpujaa@users.noreply.github.com>
2022-06-20 12:20:05 +0530

    Time: 15 ms (97.95%), Space: 19.7 MB (55.48%) - LeetHub

--- 822-card-flipping-game/822-card-flipping-game.cpp
+++ 822-card-flipping-game/822-card-flipping-game.cpp
@@ -1,59 +1,28 @@
 class Solution {
 public:
     int flipgame(vector<int>& fronts, vector<int>& backs) {
-        multiset<pair<int, int>> mt;
+        set<int> same;
         
         for(int i=0;i<fronts.size();i++)
-            mt.insert({fronts[i], backs[i]});
-        
-        int ansfront=0, dont=0, ansback=0;
-        
-        for(auto it:mt)
         {
-            if(it.first!=it.second && dont!=it.first)
-            {
-                if(ansfront==0)
-                    ansfront=it.first;
-                else if(ansfront!=it.first)
-                    break;
-            }
-            else if(ansfront!=0 && ansfront!=it.first)
-                    break;
-            else
-            {
-                dont=it.first;
-                ansfront=0;
-            }
+            if(fronts[i]==backs[i])
+                same.insert(fronts[i]);
         }
         
-        mt.clear();
+        int ans=INT_MAX;
         
         for(int i=0;i<fronts.size();i++)
-            mt.insert({backs[i], fronts[i]});
-        
-        for(auto it:mt)
         {
-            if(it.first!=it.second && dont!=it.first)
-            {
-                if(ansback==0)
-                    ansback=it.first;
-                else if(ansback!=it.first)
-                    break;
-            }
-            else if(ansback!=0 && ansback!=it.first)
-                    break;
-            else
-            {
-                dont=it.first;
-                ansback=0;
-            }
-        }
-        
-        if(ansfront==0)
-            return ansback;
-        else if(ansback==0)
-            return ansfront;
+            if(same.find(fronts[i])==same.end())
+            ans=min(ans, fronts[i]);
             
-        return (ansfront<ansback)? ansfront : ansback;
+            if(same.find(backs[i])==same.end())
+            ans=min(ans, backs[i]);
+        }
+        
+        if(ans==INT_MAX)
+            return 0;
+        
+        return ans;
     }
 };
