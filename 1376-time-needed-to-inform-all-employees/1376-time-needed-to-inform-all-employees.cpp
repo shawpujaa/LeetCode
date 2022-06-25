cd9d7cfb8a0a048e9261bf1ab5ab34a24e993356
PUJA SHAW <95918174+shawpujaa@users.noreply.github.com>
2022-06-25 12:08:58 +0530

    Time: 235 ms (93.98%), Space: 90.7 MB (95.93%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -20,8 +20,8 @@ public:
         return mxTime;
     } */
     
-    // TC-O(NLogN)
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+    // TC-O(NLogN) SC-O(N)
+   /* int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         unordered_set<int> st;
         
         for(int i=0;i<n;i++)
@@ -41,6 +41,30 @@ public:
             }
             
             mxTime=max(mxTime, time);
+        }
+        
+        return mxTime;
+    } */
+    
+    // TC-O(N) SC-O(1)
+    
+    int dfs_Time(int idx, vector<int> &manager, vector<int> &informTime)
+    {
+        if(manager[idx]!=-1)
+        {
+            informTime[idx]+=dfs_Time(manager[idx], manager, informTime);
+            manager[idx]=-1;
+        }
+        
+        return informTime[idx];
+    }
+    
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        int mxTime=-1;
+        
+        for(int i=0;i<n;i++)
+        {
+            mxTime=max(mxTime, dfs_Time(i, manager, informTime));
         }
         
         return mxTime;
