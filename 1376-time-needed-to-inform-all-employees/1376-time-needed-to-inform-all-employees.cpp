bb336f6f391cebba86e84a6ff5676aea4d569d6a
PUJA SHAW <95918174+shawpujaa@users.noreply.github.com>
2022-06-25 11:52:14 +0530

    Time: 1176 ms (5.00%), Space: 122.7 MB (48.82%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,11 +1,39 @@
 class Solution {
 public:
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+   /*  TC-O(N^2)
+   
+   int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         int mxTime=-1;
         
         for(int i=0;i<n;i++)
         {
             int j=i, time=0;
+            while(j!=headID)
+            {
+                j=manager[j];
+                time+=informTime[j];
+            }
+            
+            mxTime=max(mxTime, time);
+        }
+        
+        return mxTime;
+    } */
+    
+    // TC-O(NLogN)
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        unordered_set<int> st;
+        
+        for(int i=0;i<n;i++)
+            st.insert(i);
+        
+        for(int i=0;i<n;i++)
+            st.erase(manager[i]);
+        
+        int mxTime=-1;
+        for(auto it:st)
+        {
+            int j=it, time=0;
             while(j!=headID)
             {
                 j=manager[j];
