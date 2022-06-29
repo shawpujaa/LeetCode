466f7b525db890f0192a6bd8d914d2823b144c59
PUJA SHAW <95918174+shawpujaa@users.noreply.github.com>
2022-06-29 13:32:31 +0530

    Time: 7 ms (65.71%), Space: 11.2 MB (26.92%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -11,86 +11,30 @@
  */
 class Solution {
 public:
-       // Brute force TC O(N^2)
-    
-    /*int ans=-1;
-    void getMaxDiff(TreeNode *root, TreeNode *temp)
-    {
-        if(!temp)
-            return;
-        
-        ans=max(ans, abs(root->val-temp->val));
-        
-        getMaxDiff(root, temp->left);
-        getMaxDiff(root, temp->right);
-    }
-    
-    int maxAncestorDiff(TreeNode* root) {
-        if(!root)
-            return 0;
-        
-        getMaxDiff(root, root);
-        
-        maxAncestorDiff(root->left);
-        maxAncestorDiff(root->right);
-        
-        return ans;
-    }
-    */
-    
-    // Optmised || TC O(N) || DFS Post-Order (Bottom-Up Approach)
-    int diff=-1;
-    
-    pair<int, int> getMaxDiff(TreeNode *root)
+    int diff=0;
+    pair<int, int> getDiff(TreeNode* root)
     {
         if(!root)
             return {INT_MAX, INT_MIN};
         
-        auto [leftMin, leftMax]=getMaxDiff(root->left);
-        auto [rightMin, rightMax]=getMaxDiff(root->right);
+        auto leftt=getDiff(root->left);
+        auto rightt=getDiff(root->right);
         
-        if(leftMin!=INT_MAX)
-            diff=max({diff, abs(leftMin-root->val), abs(leftMax-root->val)});
+        leftt.first=min(leftt.first, rightt.first);
+        leftt.second=max(leftt.second, rightt.second);
         
-        if(rightMin!=INT_MAX)
-            diff=max({diff, abs(rightMin-root->val), abs(rightMax-root->val)});
+        if(leftt.first!=INT_MAX)
+        diff=max(diff, max(abs(leftt.first-root->val), abs(leftt.second-root->val)));
         
-        leftMin=min({leftMin, rightMin, root->val});
-        leftMax=max({leftMax, rightMax, root->val});
+        leftt.first=min(leftt.first, root->val);
+        leftt.second=max(leftt.second, root->val);
         
-        return {leftMin, leftMax};
+        return leftt;
     }
     
-    int maxAncestorDiff(TreeNode *root)
-    {
-        
-        getMaxDiff(root);
+    int maxAncestorDiff(TreeNode* root) {
+        getDiff(root);
         
         return diff;
     }
-    
-    
-    // Optmised || TC O(N) || DFS Pre-Order (Top-Down Approach)
-    /*int diff=-1;
-    
-    void getMaxDiff(TreeNode *root, int maxAncestor, int minAncestor)
-    {
-        if(!root)
-            return;
-        
-        diff=max({abs(maxAncestor-root->val), abs(minAncestor-root->val), diff});
-        
-        maxAncestor=max(maxAncestor, root->val);
-        minAncestor=min(minAncestor, root->val);
-        
-        getMaxDiff(root->left, maxAncestor, minAncestor);
-        getMaxDiff(root->right, maxAncestor, minAncestor);
-    }
-    
-    int maxAncestorDiff(TreeNode *root)
-    {
-        getMaxDiff(root, root->val, root->val);
-        
-        return diff;
-    }*/
 };
