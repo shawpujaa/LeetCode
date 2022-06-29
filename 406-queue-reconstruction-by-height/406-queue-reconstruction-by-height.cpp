9c5b9c1f9616cd7b1bf517cf550fd5682acb5151
PUJA SHAW <95918174+shawpujaa@users.noreply.github.com>
2022-06-29 12:01:59 +0530

    Time: 368 ms (10.17%), Space: 11.9 MB (73.09%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
-    static bool sortcol(const vector<int>& v1, const vector<int>& v2)
+    static bool sortcol(const vector<int> &v1, const vector<int> &v2)
     {
         return v1[1] < v2[1];
     }
     
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        vector<vector<int>> height;
+        
         sort(people.begin(), people.end(), sortcol);
-        
-        vector<vector<int>> res;
-        res.push_back(people[0]);
-        
-        for(int i=1;i<people.size();i++)
+       /* 
+        for(int i=0;i<people.size();i++)
         {
-            int x=people[i][1], j=0;
-            for(;j<res.size();j++)
+            cout<<people[i][0]<<" "<<people[i][1]<<"\n";
+        }*/
+        
+        for(int i=0;i<people.size();i++)
+        {
+            int x=0, j=0;
+            while(j<height.size())
             {
-                if(res[j][0]>=people[i][0])
-                    x--;
+                if(height[j][0]>=people[i][0])
+                {
+                    if(x<people[i][1])
+                        x++;
+                    else
+                        break;
+                }
                 
-                if(x<0)
-                    break;
-                
+                j++;
             }
-            
-            res.insert(res.begin()+j, people[i]);
+     
+            height.insert(height.begin()+j, people[i]);
         }
         
-        return res;
+        return height;
     }
 };
