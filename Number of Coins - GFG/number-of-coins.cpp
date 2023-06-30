c65357c590b5b23a1f277b37c24a1ec5d0a4e869
PUJA SHAW <95918174+shawpujaa@users.noreply.github.com>
2023-06-30 13:12:13 +0530

    Added solution - LeetHub

---
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	/*int minCoins(int coins[], int M, int V) 
+	{ 
+	    // Your code goes here
+	    
+	    if(V==0)
+	    return 0;
+	    
+	    int mn=INT_MAX;
+	    
+	    for(int i=0;i<M;i++)
+	    {
+	        if(coins[i] <= V)
+	        mn=min(mn, minCoins(coins, M, V-coins[i]));
+	    }
+	    
+	    if(mn==INT_MAX)
+	    return mn;
+	    
+	    return mn+1;
+	    
+	}  */
+	
+	int cntCoins(int coins[], int M, int V, vector<int>& dummy)
+	{
+	    if(V==0)
+	    return 0;
+	    
+	    if(dummy[V] != -1)
+	    return dummy[V];
+	    
+	    int mn=INT_MAX;
+	    
+	    for(int i=0;i<M;i++)
+	    {
+	        if(coins[i] <= V)
+	        mn=min(mn, cntCoins(coins, M, V-coins[i], dummy));
+	    }
+	    
+	    if(mn!=INT_MAX)
+	    dummy[V]=mn+1;
+	    else
+	    dummy[V]=mn;
+	    
+	    return dummy[V];
+	    
+	}
+	
+	int minCoins(int coins[], int M, int V)
+	{
+	    vector<int> dummy(V+1, -1);
+	    
+	    int cnt = cntCoins(coins, M, V, dummy);
+	    
+	    if(cnt==INT_MAX)
+	    return -1;
+	    
+	    return cnt;
+	    
+	    
+	}  
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int v, m;
+        cin >> v >> m;
+
+        int coins[m];
+        for(int i = 0; i < m; i++)
+        	cin >> coins[i];
+
+      
+	    Solution ob;
+	    cout << ob.minCoins(coins, m, v) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
